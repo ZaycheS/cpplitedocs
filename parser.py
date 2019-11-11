cd82e18d70bf1ff55ea25c1eb231df0d8e494860
ZaycheS <sergey.zaychenko.12@gmail.com>
2019-11-11 18:12:40 +0200

    parenthesesSkip/include define list

--- parser.py
+++ parser.py
@@ -1,3 +1,50 @@
-file = open(r'C:\Users\serzh\PycharmProjects\untitled\testing.py', 'r')
-strings = file.read()
-print(strings.find('print'))
+open_list = ["[", "{", "("]
+close_list = ["]", "}", ")"]
+
+
+def parenthesesSkip(strings, i, par='{'):
+    stack = [par]
+    start = True
+    while stack and i<=len(strings):
+        for j in strings[i]:
+            if j in open_list:
+                if not start:
+                    stack.append(j)
+                else:
+                    start = False
+            elif j in close_list:
+                pos = close_list.index(j)
+                if ((len(stack) > 0) and
+                        (open_list[pos] == stack[len(stack) - 1])):
+                    stack.pop()
+                else:
+                    return "Unbalanced"
+        if stack:
+            i+=1
+    return i+1
+
+file = open(r'D:\code.cpp', 'r')
+strings = file.readlines()
+comment = False
+includes = list()
+
+print(parenthesesSkip(strings,53,'('))
+for i in range(len(strings)):
+    if comment:
+        if strings[i].endswith('*/\n'):
+            comment = False
+        continue
+    if strings[i].startswith('/*'):
+        comment = True
+        continue
+    if strings[i].startswith('#include'):
+        include = strings[i].replace('\n', '')
+        includes.append(include)
+        continue
+    if strings[i].startswith('#define'):
+        include = strings[i].replace('\n', '')
+        includes.append(include)
+        continue
+    # print(strings[i])
+for i in range(len(includes)):
+    print(includes[i])
